1561d779725fca072cfdfe85b0571d8dbe103de2
votti <mcvotti@gmail.com>
2020-06-05 21:02:00 +0200

    Adapt scripts to new imctools

--- workflow/scripts/convert_imcfolder.py
+++ workflow/scripts/convert_imcfolder.py
@@ -1,4 +1,4 @@
-from imctools.scripts import convertfolder2imcfolder
+from imctools.converters import mcdfolder2imcfolder
 import pathlib
 import traceback
 
@@ -6,12 +6,11 @@ if __name__ == '__main__':
     fn_zip = snakemake.input.fn_zip
     fol_ome = snakemake.params.fol_ome
     try:
-        convertfolder2imcfolder.convert_folder2imcfolder(str(fn_zip),
-                out_folder=str(fol_ome),
-                dozip=False)
+        mcdfolder2imcfolder.mcdfolder_to_imcfolder(
+	        str(fn_zip), output_folder=str(fol_ome),
+            create_zip=False)
     except:
         fn_failed = pathlib.Path(fol_ome) / (pathlib.Path(fn_zip).stem + '.failed')
         with open(fn_failed, 'w') as f:
             traceback.print_exc(file=f)
-
 
